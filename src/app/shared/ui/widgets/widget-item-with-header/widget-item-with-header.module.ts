18a0942d7c042271ede8b1e9de8eaa66b1a6f258
alexeyGoncharovEnconso <alexey.goncharov@enconso.com>
2021-12-05 19:10:12 +0300

    change menu button

--- src/app/shared/ui/widgets/widget-item-with-header/widget-item-with-header.module.ts
+++ src/app/shared/ui/widgets/widget-item-with-header/widget-item-with-header.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WidgetItemWithHeaderComponent } from './widget-item-with-header.component';
 import { RoundGaugeModule } from '../../round-gauge/round-gauge.module';
+import {MatIconModule} from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   imports: [
     CommonModule,
     RoundGaugeModule,
+    MatIconModule,
+    MatTooltipModule,
   ],
   declarations: [WidgetItemWithHeaderComponent],
   exports: [WidgetItemWithHeaderComponent]
